a11bb5301a65e98a0e0d49d728bf86d1201074b5
DellaPutriHerman <127371139+dellaputriherman@users.noreply.github.com>
2024-03-28 15:51:58 +0700

    Upload Microservice (Dapartement-Service)

---
+++ dapartement-service/src/main/java/com/della/dapartementservice/DapartementServiceApplication.java
@@ -0,0 +1,14 @@
+package com.della.dapartementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class DapartementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DapartementServiceApplication.class, args);
+	}
+
+}
---
+++ dapartement-service/src/test/java/com/della/dapartementservice/DapartementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.della.dapartementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DapartementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
